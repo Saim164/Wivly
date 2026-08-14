0c4ef0f637b80d907c586ba8ba663cc4717e7492
Saim <majesticpainter79@gmail.com>
2026-08-14 12:21:34 +0500

    Post creation logic added

--- backend/controllers/post.controllers.js
+++ backend/controllers/post.controllers.js
@@ -1,7 +1,29 @@
+const Post = require("../models/post.model.js");
+
 const activeCheck = async (req, res) => {
   return res.status(200).json({ message: "Active" });
 };
 
+const createPost = async (req, res) => {
+  const user = req.user;
+  const { body } = req.body;
+  try {
+    const post = new Post({
+      userId: user._id,
+      body: body,
+      media: req.file !== undefined ? req.file.filename : "",
+      fileType: req.file !== undefined ? req.file.mimetype.split("/")[1] : "",
+    });
+
+    await post.save();
+
+    return res.status(200).json({ message: "Post created" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   activeCheck,
+  createPost
 };
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const authMiddleware = require("../middlewares/auth.js");
+const multer = require("multer");
+const { activeCheck, createPost } = require("../controllers/post.controllers");
 
-const { activeCheck } = require("../controllers/post.controllers");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.route("/").get(activeCheck);
+router.route("/create-post").post(authMiddleware, upload.single("media"), createPost);
 
 module.exports = router;
